9747887054f0660875f3b08ff806aef7f9c366db
vallshebnik <vall.shebnik@mail.ru>
2017-09-22 11:09:25 +0200

    Add files via upload

---
+++ Work.php
@@ -0,0 +1,12 @@
+<?hph errorr_reporting(0)>//zapros
+$a=$_REQUEST["a"];
+$b=$_REQUEST["b"];
+$oper=$_REQUEST["oper"];
+//raschot
+if($oper=="1")$d=$a+$b;
+elseif($oper=="2")$d=$a-$b;
+elseif($oper=="3")$d=$a*$b;
+elseif($oper=="4")$d=$a/$b;
+//vivod
+echo $4;
+?>
